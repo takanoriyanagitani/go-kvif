b96df890b01bcd3546655da19575edca1eaaa695
yanagitani <takanori.yanagitani@gmail.com>
2022-09-22 15:21:55 +0900

    feat: reader at sized construction

--- key.go
+++ key.go
@@ -5,5 +5,12 @@ type Key struct {
 	raw    []byte
 }
 
+func KeyNew(bucket string, id []byte) Key {
+	return Key{
+		bucket: bucket,
+		raw:    id,
+	}
+}
+
 func (k Key) Bucket() string { return k.bucket }
 func (k Key) Raw() []byte    { return k.raw }
--- pkg/fs/sized.go
+++ pkg/fs/sized.go
@@ -1,12 +1,25 @@
 package kvfs
 
 import (
+	"bytes"
 	"io"
 )
 
 type ReaderAtSized struct {
 	ra io.ReaderAt
 	sz int64
+}
+
+func ReaderAtSizedNew(ra io.ReaderAt, sz int64) ReaderAtSized {
+	return ReaderAtSized{
+		ra,
+		sz,
+	}
+}
+
+func ReaderAtSizedFromBytes(b []byte) ReaderAtSized {
+	var rdr *bytes.Reader = bytes.NewReader(b)
+	return ReaderAtSizedNew(rdr, rdr.Size())
 }
 
 func (r ReaderAtSized) ReaderAt() io.ReaderAt { return r.ra }
