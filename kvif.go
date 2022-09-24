dfd5af00fca162eb90474c0825e7ec45799081cb
yanagitani <takanori.yanagitani@gmail.com>
2022-09-24 20:55:05 +0000

    feat: lst I/F

--- kvif.go
+++ kvif.go
@@ -9,6 +9,9 @@ type Kv interface {
 	// If val not exists, error must be ErrNotFound
 	Get(ctx context.Context, key Key) (Val, error)
 
+	// Lst tries to get keys in a bucket.
+	Lst(ctx context.Context, bucket string) (keys Iter[Key], err error)
+
 	// Close closes something(optional).
 	// Caller must call close.
 	// Some implementation may use idempotent close.
